a3b6d23a70645f8458523d3fb8f00457e06fec05
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-09-29 17:35:51 -0300

    company name

--- src/components/company-card.jsx
+++ src/components/company-card.jsx
@@ -1,8 +1,9 @@
 // eslint-disable-next-line react/prop-types
-export function CompanyCard({ src, jobRole, alternative}) {
+export function CompanyCard({ src, jobRole, alternative, companyName}) {
     return (
         <div>
             <img src={src} alt={alternative} />
+            <h3>{companyName}</h3>
             <h4>{jobRole}</h4>
         </div>
     )
--- src/sections/companies/index.jsx
+++ src/sections/companies/index.jsx
@@ -11,9 +11,9 @@ export function Companies() {
         <section className="companies-container">
             <h1>Companies I worked for</h1>
             <div className="companies-list">
-                <CompanyCard src={exxonMobil} jobRole='Software Developer' alternative='Exxon Mobil company logo'/>
-                <CompanyCard src={concentrix} jobRole='Software Developer' alternative='Concentrix company logo'/>
-                <CompanyCard src={madero} jobRole='Intern' alternative='Madero company logo'/>
+                <CompanyCard companyName='ExxonMobil' src={exxonMobil} jobRole='Software Developer' alternative='Exxon Mobil company logo'/>
+                <CompanyCard companyName='Concentrix' src={concentrix} jobRole='Software Developer' alternative='Concentrix company logo'/>
+                <CompanyCard companyName='Madero' src={madero} jobRole='Intern' alternative='Madero company logo'/>
             </div>
         </section>
     )
